f5446ee5d73a32c9d7e97f2aaea8ab54586b7d40
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-17 15:49:39 +0500

    Updates

---
+++ precache-manifest.21f98d057f48721994c129f54c6f3ca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5525cbeb885102fdc9c532d9eb5fee8",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "c361a8373c88f71623a7",
+    "url": "/jokes/static/css/main.d8ddb773.chunk.css"
+  },
+  {
+    "revision": "f168187fc29f33d3389d",
+    "url": "/jokes/static/js/2.a13261f0.chunk.js"
+  },
+  {
+    "revision": "c361a8373c88f71623a7",
+    "url": "/jokes/static/js/main.19034375.chunk.js"
+  },
+  {
+    "revision": "8811f36da1d3b03f81ba",
+    "url": "/jokes/static/js/runtime~main.4ae1665c.js"
+  }
+]);
